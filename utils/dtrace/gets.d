65cdfd28bc402b94373364d255892aeb11a5ecf5
Alvaro Videla <videlalvaro@gmail.com>
2011-09-17 18:40:26 +0200

    fixes gets scripts for new command version

--- utils/dtrace/gets.d
+++ utils/dtrace/gets.d
@@ -3,7 +3,8 @@
  * Sample script to trace syscalls that happen during a GET command.
  * Run it like this: sudo dtrace -qs gets.d -p `pgrep redis`
  */
-redis$target:::get-entry
+redis$target:::command-entry
+/copyinstr(arg0) == "get"/
 {
   self->traceIt = 1;
   printf("get called with key: %s\n", copyinstr(arg0));
@@ -15,7 +16,8 @@ syscall:::entry
   printf("%s:%s\n", probemod, probefunc);
 }
 
-redis$target:::get-return
+redis$target:::command-return
+/copyinstr(arg0) == "get"/
 {
   self->traceIt = 0;
   printf("get returned: %d for key:%s\n", arg1, copyinstr(arg0));
